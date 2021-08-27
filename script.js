f9276a8270e4cbfe9856a653dfd5903bad56a76b
Jan Błażejczyk <JB.Blazejczyk@gmail.com>
2021-08-28 00:48:51 +0200

    functions for calculations

---
+++ script.js
@@ -0,0 +1,53 @@
+// functions for basic math operations
+const add = (a, b) => {
+    return a + b;
+}
+
+const subtract = (a, b) => {
+    return a - b;
+}
+
+const multiply = (a, b) => {
+    return a * b;
+}
+
+const divide = (a, b) => {
+    if (b !== 0) {
+        return a / b;
+    }
+    else {
+        return "Can't divide by 0!"
+    }
+
+}
+
+const getPercent = (a) => {
+    return a / 100;
+}
+
+const changeSign = (a) => {
+    if (a > 0) {
+        return -a;
+    }
+    else {
+        return a;
+    }
+}
+
+// function for performing an operation
+// takes two numbers and an operator
+// returns the result of the operation
+const operator = (a, operator, b) => {
+    if (operator === "+") {
+        return add(a, b);
+    }
+    else if (operator === "-") {
+        return subtract(a, b);
+    }
+    else if (operator === "*") {
+        return multiply(a, b);
+    }
+    else if (operator === "/") {
+        return divide(a, b);
+    }
+}
